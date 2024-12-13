04c9adf64abd8fa7202d17bbc9fbab137e27320c
abene <abenezertileye2003@gmail.com>
2024-12-13 10:51:27 +0300

    auth

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user_controller')
-const upload = require('../middleware/file_upload_config');
+const authController = require('../controllers/auth/user_auth_controller')
 
 // GET all post
 router.get("/get-all-busses", controller.getAllSchedules);
 router.post("/buy-ticket/:id", controller.buyTicket);
+//signup
+router.post('/signup', authController.signup)
+//login
+router.post('/login', authController.login)
 
 // Export the router
 module.exports = router;
