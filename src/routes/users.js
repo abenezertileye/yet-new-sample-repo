5a2be8727f43bf6f951a1ac2387d33089a5eee47
abene <abenezertileye2003@gmail.com>
2024-12-13 10:09:49 +0300

    controller

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/user_controller')
 const upload = require('../middleware/file_upload_config');
 
 // GET all post
-router.get("/get-all-busses", controller.getAllBuses);
+router.get("/get-all-busses", controller.getAllSchedules);
 router.post("/buy-ticket/:id", controller.buyTicket);
 
 // Export the router
