46ab73ee0582f4b887163a1d299673bdcc3fa94f
abene <abenezertileye2003@gmail.com>
2024-12-13 09:54:42 +0300

    general setup

--- src/models/bus.js
+++ src/models/bus.js
@@ -6,6 +6,8 @@ const BusSchema = new Schema({
   bus_number: { type: String, required: true, maxLength: 100 },
   plate_number: { type: String, required: true, maxLength: 100 },
   state: { type: String, required: true, maxLength: 100 },
+    tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ticket" }], 
+  
 });
 
 const Bus = mongoose.model('Bus', BusSchema)
