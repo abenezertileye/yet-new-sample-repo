a83b6200179b6a55d632c5dfdfb0a54a98106655
abene <abenezertileye2003@gmail.com>
2024-12-13 10:51:01 +0300

    auth

---
+++ src/controllers/auth/user_auth_controller.js
@@ -0,0 +1,44 @@
+const User = require('../../models/user')
+const jwt = require('jsonwebtoken')
+
+//create token
+let MaxAge = 3*24*60*60
+function createToken(id){
+    return jwt.sign({id}, 'osjcosj56as', {expiresIn:MaxAge})
+}
+
+//signup
+exports.signup = async(req, res) => {
+    const {first_name, last_name, email, password} = req.body
+    try{
+        const user = await User.create({first_name,last_name, email, password})
+        const token = createToken(user._id)
+        res.send(
+            {first_name:first_name,
+            last_name: last_name,
+            email:email,
+            token:token,}
+        )
+    }catch(err){
+        console.log(err.message)
+    }
+}
+
+//login
+exports.login = async function(req, res){
+    const {email, password} = req.body
+    try{
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        const theUser = await User.findById(user._id);
+        res.send(
+            {first_name:theUser.first_name,
+                last_name: theUser.last_name,
+            email:email,
+            token:token,}
+        )
+        
+    }catch(err){
+        res.send(err.message)
+    }
+}
