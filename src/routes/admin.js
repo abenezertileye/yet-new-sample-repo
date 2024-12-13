c99c32fc622d5ba735515e910bac1622c9e2ff1f
abene <abenezertileye2003@gmail.com>
2024-12-13 10:09:44 +0300

    controller

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,8 +6,8 @@ router.post("/create-schedule", controller.createSchedule);
 router.patch("/get-all-drivers", controller.getDrivers);
 router.patch("/get-all-helpers", controller.get);
 router.patch("/get-all-routes", controller.updatePost);
-router.patch("/get-all-busses", controller.updatePost);
-router.patch("/get-all-schedule", controller.updatePost);
+router.patch("/get-all-busses", controller.getAllBuses);
+router.patch("/get-all-schedule", controller.getAllSchedules);
 
 
 
