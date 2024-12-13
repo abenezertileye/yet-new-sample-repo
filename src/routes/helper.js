3fa32f3f7cc473a719ebd7e5bb0447b32063dca2
abene <abenezertileye2003@gmail.com>
2024-12-13 10:51:23 +0300

    auth

--- src/routes/helper.js
+++ src/routes/helper.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const controller = require('../controllers/helper_controller')
+const controller = require('../controllers/auth/helper_controller')
 
 //signup
 router.post('/signup', controller.signup)
