ae1e8f116959e6bd5a0bf202863a00d1cc674d20
abene <abenezertileye2003@gmail.com>
2024-12-19 11:06:39 +0300

    fixes

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -2,21 +2,22 @@ const Schedule = require('../models/schedule')
 const Bus = require('../models/bus')
 const Driver = require('../models/driver')
 const Helper = require('../models/helper')
+const Route = require('../models/route')
 
 exports.createSchedule = async (req, res) => {
-  const { schedule_name, driverId, helperId, routeId, busId, date, laps, expected_income } = req.body;
+  const { schedule_name, driverId, helperId, routeId, busId, laps, expected_income } = req.body;
   const adminId = req.user.id; // Assuming middleware to extract admin's ID
 
   try {
     // Validate required fields
-    if (!driverId || !helperId || !routeId || !busId || !date || !laps || !expected_income) {
+    if (!driverId || !helperId || !routeId || !busId || !laps || !expected_income) {
       return res.status(400).json({ message: "All fields are required." });
     }
 
     // Create the schedule
     const newSchedule = await Schedule.create({
       schedule_name,
-      date,
+      date: new Date(),
       laps,
       expected_income,
       bus: busId,
@@ -36,6 +37,73 @@ exports.createSchedule = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "An error occurred while creating the schedule",
+      error: err.message,
+    });
+  }
+};
+
+//new route
+exports.createRoute = async (req, res) => {
+  const { route_name, route} = req.body;
+  const adminId = req.user.id; // Assuming middleware to extract admin's ID
+
+  try {
+    // Validate required fields
+    if (!route_name || !route) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // Create the schedule
+    const newRoute = await Route.create({
+      routeName: route_name,
+      route,
+      createdBy: adminId,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Route created successfully",
+      new_route: newRoute,
+    });
+  } catch (err) {
+    console.error("Error creating route:", err.message);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while creating the route",
+      error: err.message,
+    });
+  }
+};
+//new bus
+exports.createBus = async (req, res) => {
+  const { bus_number, plate_number} = req.body;
+  const adminId = req.user.id; // Assuming middleware to extract admin's ID
+
+  try {
+    // Validate required fields
+    if (!bus_number || !plate_number) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // Create the schedule
+    const newBus = await Bus.create({
+      busNumber: bus_number,
+      plateNumber: plate_number,
+      state: "new",
+      tickets: [],
+      createdBy: adminId,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Bus created successfully",
+      new_route: newBus,
+    });
+  } catch (err) {
+    console.error("Error creating bus:", err.message);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while creating the bus",
       error: err.message,
     });
   }
@@ -126,44 +194,23 @@ exports.getAllHelper= async (req, res) => {
       });
     }
   };
-exports.getAllHelper= async (req, res) => {
-    try {
-      // Retrieve all JobPost documents from the database
-      const helper = await Helper.find();
-  
-      // Send the retrieved helper as the response
-      res.status(200).json({
-        success: true,
-        data: helper,
-        message: "All helper retrieved successfully",
-      });
-    } catch (error) {
-      // Handle any errors that occur during retrieval
-      console.error("Error retrieving helper:", error.message);
-      res.status(500).json({
-        success: false,
-        message: "Failed to retrieve helper",
-        error: error.message,
-      });
-    }
-  };
 exports.getAllRoute= async (req, res) => {
     try {
       // Retrieve all JobPost documents from the database
-      const helper = await Helper.find();
+      const route = await Route.find();
   
-      // Send the retrieved helper as the response
+      // Send the retrieved route as the response
       res.status(200).json({
         success: true,
-        data: helper,
-        message: "All helper retrieved successfully",
+        data: route,
+        message: "All route retrieved successfully",
       });
     } catch (error) {
       // Handle any errors that occur during retrieval
-      console.error("Error retrieving helper:", error.message);
+      console.error("Error retrieving route:", error.message);
       res.status(500).json({
         success: false,
-        message: "Failed to retrieve helper",
+        message: "Failed to retrieve route",
         error: error.message,
       });
     }
