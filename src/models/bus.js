1f3ba7d9de402286e89eb9ffd0329c2d940b6014
abene <abenezertileye2003@gmail.com>
2024-12-19 11:06:48 +0300

    fixes

--- src/models/bus.js
+++ src/models/bus.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt')
 
 const BusSchema = new Schema({
-  bus_number: { type: String, required: true, maxLength: 100 },
-  plate_number: { type: String, required: true, maxLength: 100 },
+  busNumber: { type: String, required: true, maxLength: 100 },
+  plateNumber: { type: String, required: true, maxLength: 100 },
   state: { type: String, required: true, maxLength: 100 },
     tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ticket" }], 
   
