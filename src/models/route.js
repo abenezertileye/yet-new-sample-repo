41d7456b7debb53d36e2f61253b63ef3c7a6ec81
abene <abenezertileye2003@gmail.com>
2024-12-19 11:06:56 +0300

    fixes

--- src/models/route.js
+++ src/models/route.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RouteSchema = new Schema({
+  routeName: { type: String, required: true, maxLength: 100 },
   route: { type: Array, required: true, maxLength: 100 },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" }, 
 
 });
 
