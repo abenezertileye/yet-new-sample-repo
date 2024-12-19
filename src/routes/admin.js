9e0df59e08a36b68ade4619be3b9254db0b3c59c
abene <abenezertileye2003@gmail.com>
2024-12-19 11:07:05 +0300

    fixes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const controller = require('../controllers/admin_controller')
 
 router.post("/create-schedule", controller.createSchedule);
-router.patch("/get-all-drivers", controller.getAllDriver);
-router.patch("/get-all-helpers", controller.getAllHelper);
-router.patch("/get-all-routes", controller.getAllRoute);
-router.patch("/get-all-busses", controller.getAllBuses);
-router.patch("/get-all-schedule", controller.getAllSchedules);
+router.post("/create-route", controller.createRoute);
+router.post("/create-bus", controller.createBus);
+router.get("/get-all-drivers", controller.getAllDriver);
+router.get("/get-all-helpers", controller.getAllHelper);
+router.get("/get-all-routes", controller.getAllRoute);
+router.get("/get-all-busses", controller.getAllBuses);
+router.get("/get-all-schedule", controller.getAllSchedules);
 
 
 
