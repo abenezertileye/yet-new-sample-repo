5df3dc83c7faf48ec071ce557d951ffb3963ce3d
abene <abenezertileye2003@gmail.com>
2025-02-04 16:10:02 +0300

    new functionalities

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,9 @@ const controller = require('../controllers/user_controller')
 const authController = require('../controllers/auth/user_auth_controller')
 
 // GET all post
-router.get("/get-all-routes", controller.getAllRoute);
+router.get("/get-all-schedule", controller.getAllSchedule);
 router.post("/buy-ticket/:id", controller.buyTicket);
+router.get("/get-user-info/:id", controller.getUserInfo);
 //signup
 router.post('/signup', authController.signup)
 //login
